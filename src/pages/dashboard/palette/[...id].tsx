c3d67491d754821049d1e048d17c4ac2bde2a135
Alex Tirim <alexandru.tirim@yahoo.com>
2023-02-22 20:07:30 +0200

    -

---
+++ src/pages/dashboard/palette/[...id].tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SinglePalette = () => {
+  return (
+    <main>
+      <section className="p-full">SinglePalette</section>
+    </main>
+  );
+};
+
+export default SinglePalette;
