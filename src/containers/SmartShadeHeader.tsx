d8f139c6d78a0f8ffc5e6526750fa193e77a03a7
Alex Tirim <alexandru.tirim@yahoo.com>
2023-02-23 16:33:28 +0200

    -

--- src/containers/SmartShadeHeader.tsx
+++ src/containers/SmartShadeHeader.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import { TypographyP } from "@/components/Typography";
 import { Label } from "@/components/Label";
 import { Switch } from "@/components/Switch";
+import { TypographyP } from "@/components/Typography";
 
 type Props = {
   colorName: string;
@@ -10,14 +10,19 @@ type Props = {
 
 const SmartShadeHeader = ({ colorName }: Props) => {
   return (
-    <div className="flex flex-row justify-between items-center w-full md:w-2/3">
-    <TypographyP className="min-w-[6rem] font-semibold capitalize md:min-w-[8rem] md:text-xl">
-      {colorName}
-    </TypographyP>
-    <div className="flex flex-row items-center gap-2">
-      <Switch id={`autogenerate-shades-${colorName}`} />
-      <Label className="md:text-md" htmlFor={`autogenerate-shades-${colorName}`}>Autogenerate Shades</Label>
-    </div>
+    <div className="flex w-full flex-row items-center justify-between md:w-1/2">
+      <TypographyP className="min-w-[6rem] font-semibold capitalize md:min-w-[8rem] md:text-xl">
+        {colorName}
+      </TypographyP>
+      <div className="flex flex-row items-center gap-2">
+        <Switch id={`autogenerate-shades-${colorName}`} />
+        <Label
+          className="md:text-md cursor-pointer"
+          htmlFor={`autogenerate-shades-${colorName}`}
+        >
+          Autogenerate Shades
+        </Label>
+      </div>
     </div>
   );
 };
