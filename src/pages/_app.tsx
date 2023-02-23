26671389f42fb5642287e2d6e0b69d1169228ef7
Alex Tirim <alexandru.tirim@yahoo.com>
2023-02-23 14:40:23 +0200

    -

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,17 +7,29 @@ import {
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { useState } from "react";
 import "src/styles/globals.css";
 
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
-const queryClient = new QueryClient();
-
 const MyApp: AppType<{
   session: Session | null;
   dehydratedState: DehydratedState;
 }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 60 * 24 * 7, // 7 days
+            refetchOnWindowFocus: false,
+            cacheTime: 1000 * 60 * 60 * 24 * 7, // 7 days
+          },
+        },
+      })
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
