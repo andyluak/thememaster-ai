73d17cdb41be2701e70e3d1d00cacfa141e90b74
Alex Tirim <alexandru.tirim@yahoo.com>
2023-02-23 12:21:18 +0200

    -

--- src/pages/dashboard/palette/[...id].tsx
+++ src/pages/dashboard/palette/[...id].tsx
@@ -1,9 +1,16 @@
+import { CornerDownLeft } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const SinglePalette = () => {
   return (
     <main>
-      <section className="p-full">SinglePalette</section>
+      <section className="p-full">
+        <Link href={"/dashboard"} className="flex flex-row items-center gap-2">
+          <CornerDownLeft />
+          Back
+        </Link>
+      </section>
     </main>
   );
 };
