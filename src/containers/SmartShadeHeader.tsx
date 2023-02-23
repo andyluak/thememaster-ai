9abbc2a65df6954c1df5e5b8590e16415191012c
Alex Tirim <alexandru.tirim@yahoo.com>
2023-02-23 16:41:27 +0200

    -

--- src/containers/SmartShadeHeader.tsx
+++ src/containers/SmartShadeHeader.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 import { Label } from "@/components/Label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/Select";
 import { Switch } from "@/components/Switch";
 import { TypographyP } from "@/components/Typography";
 
@@ -10,18 +17,30 @@ type Props = {
 
 const SmartShadeHeader = ({ colorName }: Props) => {
   return (
-    <div className="flex w-full flex-row items-center justify-between md:w-1/2">
+    <div className="flex w-full flex-row items-center justify-between md:w-[55%]">
       <TypographyP className="min-w-[6rem] font-semibold capitalize md:min-w-[8rem] md:text-xl">
         {colorName}
       </TypographyP>
-      <div className="flex flex-row items-center gap-2">
-        <Switch id={`autogenerate-shades-${colorName}`} />
-        <Label
-          className="md:text-md cursor-pointer"
-          htmlFor={`autogenerate-shades-${colorName}`}
-        >
-          Autogenerate Shades
-        </Label>
+      <div className="flex flex-row items-center gap-4">
+        <div className="flex items-center space-x-2">
+          <Switch id={`autogenerate-shades-${colorName}`} />
+          <Label
+            className="md:text-md cursor-pointer"
+            htmlFor={`autogenerate-shades-${colorName}`}
+          >
+            Autogenerate Shades
+          </Label>
+        </div>
+        <Select>
+          <SelectTrigger className="w-[60px] md:w-[80px]">
+            <SelectValue placeholder="HEX" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="hex">HEX</SelectItem>
+            <SelectItem value="rgb">RGB</SelectItem>
+            <SelectItem value="hsl">HSL</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
     </div>
   );
