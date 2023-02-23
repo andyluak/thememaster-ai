db611f673531a4c1710b911f8b69932681954f54
Alex Tirim <alexandru.tirim@yahoo.com>
2023-02-23 17:13:20 +0200

    -

--- src/containers/SmartShade.tsx
+++ src/containers/SmartShade.tsx
@@ -1,6 +1,15 @@
 import type { Color } from "@prisma/client";
 import React from "react";
+import { HexColorPicker } from "react-colorful";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/Dialog";
 import { TypographyP } from "@/components/Typography";
 
 type Props = {
@@ -9,12 +18,28 @@ type Props = {
 
 const SmartShade = ({ color }: Props) => {
   const { code } = color;
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <div className="flex flex-col items-center justify-center gap-2 md:flex-row md:items-center">
-      <button
-        className="h-8 w-8 rounded-full border border-gray-600 md:h-10 md:w-10"
-        style={{ backgroundColor: code }}
-      />
+      <Dialog>
+        <DialogTrigger>
+          <button
+            className="h-8 w-8 rounded-full border border-gray-600 md:h-10 md:w-10"
+            style={{ backgroundColor: code }}
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        </DialogTrigger>
+        <DialogContent>
+          <DialogTitle>Select a new shade</DialogTitle>
+          <HexColorPicker
+            color={code}
+            onChange={(color) => console.log(color)}
+          />
+          <DialogDescription>
+            <TypographyP className="text-xs"> {code}</TypographyP>
+          </DialogDescription>
+        </DialogContent>
+      </Dialog>
       <TypographyP className="text-xs">{code}</TypographyP>
     </div>
   );
