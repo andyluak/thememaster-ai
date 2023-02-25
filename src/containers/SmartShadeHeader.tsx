dbd181c811bbfd62ea3b012fc1f0552f10127da9
Alex Tirim <alexandru.tirim@yahoo.com>
2023-02-25 19:10:18 +0200

    -

--- src/containers/SmartShadeHeader.tsx
+++ src/containers/SmartShadeHeader.tsx
@@ -3,7 +3,14 @@ import type { ColorFormatKeys } from "store/useColorOptions";
 
 import AutogenerateShadesInput from "@/components/ColorPalette/AutogenerateShadesInput";
 import ColorFormatSelect from "@/components/ColorPalette/ColorFormatSelect";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/Tooltip";
 import { TypographyP } from "@/components/Typography";
+import { HelpCircle } from "lucide-react";
 
 type Props = {
   colorName: ColorFormatKeys;
@@ -12,12 +19,22 @@ type Props = {
 
 const SmartShadeHeader = ({ colorName, paletteId }: Props) => {
   return (
-    <div className="flex w-[95%] flex-col md:flex-row md:items-center gap-2 md:gap-0 justify-between md:w-full lg:w-2/3 xl:w-3/4">
+    <div className="flex w-[95%] flex-col justify-between gap-2 md:w-full md:flex-row md:items-center md:gap-0 lg:w-2/3 xl:w-3/4">
       <TypographyP className="min-w-[6rem] font-semibold capitalize md:min-w-[8rem] md:text-xl">
         {colorName}
       </TypographyP>
-      <div className="flex flex-row justify-between items-center gap-6">
-        <AutogenerateShadesInput id={`autogenerate-shades-${colorName}`} />
+      <div className="flex flex-row items-center justify-between gap-6">
+        <div className="flex flex-row gap-1">
+          <TooltipProvider delayDuration={500}>
+            <Tooltip>
+              <TooltipTrigger><HelpCircle className="w-4 md:w-6"/></TooltipTrigger>
+              <TooltipContent>
+                <TypographyP className="text-sm">When changing the first color, the rest of the shades will be automatically generated.</TypographyP>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <AutogenerateShadesInput id={`autogenerate-shades-${colorName}`} />
+        </div>
         <ColorFormatSelect colorName={colorName} paletteId={paletteId} />
       </div>
     </div>
