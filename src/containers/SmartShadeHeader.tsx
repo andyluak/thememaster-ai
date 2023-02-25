96f0e9e22b0a004bd0e284a468fdc0b7869c07fa
Alex Tirim <alexandru.tirim@yahoo.com>
2023-02-25 18:24:50 +0200

    -

--- src/containers/SmartShadeHeader.tsx
+++ src/containers/SmartShadeHeader.tsx
@@ -12,11 +12,11 @@ type Props = {
 
 const SmartShadeHeader = ({ colorName, paletteId }: Props) => {
   return (
-    <div className="flex w-[95%] flex-row items-center justify-between md:w-full lg:w-2/3 xl:w-3/4">
+    <div className="flex w-[95%] flex-col md:flex-row md:items-center gap-2 md:gap-0 justify-between md:w-full lg:w-2/3 xl:w-3/4">
       <TypographyP className="min-w-[6rem] font-semibold capitalize md:min-w-[8rem] md:text-xl">
         {colorName}
       </TypographyP>
-      <div className="flex flex-row items-center gap-6">
+      <div className="flex flex-row justify-between items-center gap-6">
         <AutogenerateShadesInput id={`autogenerate-shades-${colorName}`} />
         <ColorFormatSelect colorName={colorName} paletteId={paletteId} />
       </div>
